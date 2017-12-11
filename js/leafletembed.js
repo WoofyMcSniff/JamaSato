40cfc4bb6df03558ab30ce72ccce9c5e5e8f434b
Malte Tiemann <malte.tiemann90@web.de>
2017-12-11 11:04:10 +0100

    new frontend desgin

--- js/leafletembed.js
+++ js/leafletembed.js
@@ -1,12 +1,6 @@
-var mymap = L.map('mapid').setView([51.96, 7.626], 13);
+var map = L.map('myMap').setView([51.96, 7.626], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', { //brauchen hier andere Base Map OSM ist zu viel
-    attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>', //dran denken die Attribution zu ändern
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //brauchen hier andere Base Map OSM ist zu viel
+    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors', //dran denken die Attribution zu ändern
     maxZoom: 18,
-}).addTo(mymap);
-
-
-var Hydda_Full = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+}).addTo(map);
