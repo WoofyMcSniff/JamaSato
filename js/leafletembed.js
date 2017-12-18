5207e4f3d270a68c7c7301386d8ae4499bcc8be1
WoofyMcSniff <malte.tiemann90@web.de>
2017-12-18 13:17:35 +0100

    Delete leafletembed.js

--- js/leafletembed.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('myMap').setView([51.96, 7.626], 13);
-
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //brauchen hier andere Base Map OSM ist zu viel
-    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors', //dran denken die Attribution zu ändern
-    maxZoom: 18,
-}).addTo(map);
