98b7ef5450460928d1cb7848e10cb906e03ccea6
WoofyMcSniff <malte.tiemann90@web.de>
2018-01-19 16:56:26 +0100

    Add files via upload
    new push for app, same as master branch

--- app/js/createtable.js
+++ app/js/createtable.js
@@ -1,4 +1,32 @@
-function onLoad(){
+var searchdata
+
+function tableButton(){
+
+
+    $('#example').DataTable( {
+			  searching: false,
+				scrollX: true,
+        "ajax": "./json/jsondata.json",
+        "columns": [
+            { "data": "name" },
+            { "data": "date" },
+            { "data": "location" }
+        ]
+    } );
+		$('#example').on('click', 'tr', function () {
+            var datastring = this.outerText;
+            var croppedData = datastring.substring(0, datastring.indexOf('.SAFE'));
+            croppedData = croppedData + '.SAFE'
+            layertomap(croppedData);
+    } );
+}
+
+function layertomap(croppedData) {
+    var pathbase = './layer/' + croppedData
+
+    console.log(pathbase);
+}
+/*function onLoad(){
 			var url='../app/metadata/metadata.json';
 			$.getJSON(url, function(json){
 				var table = $('<table>');
@@ -30,3 +58,58 @@ function onLoad(){
 				$('body').append(table);
 			});
 		}
+
+*/
+
+/*
+function tableButton() {
+
+	console.log("tableButton");
+			 $.getJSON("./json/jsondata.json", function (data) {
+
+					 var arrItems = [];      // THE ARRAY TO STORE JSON ITEMS.
+					 $.each(data, function (index, value) {
+							 arrItems.push(value);       // PUSH THE VALUES INSIDE THE ARRAY.
+					 });
+
+					 // EXTRACT VALUE FOR TABLE HEADER.
+					 var col = [];
+					 for (var i = 0; i < arrItems.length; i++) {
+							 for (var key in arrItems[i]) {
+									 if (col.indexOf(key) === -1) {
+											 col.push(key);
+									 }
+							 }
+					 }
+
+					 // CREATE DYNAMIC TABLE.
+					 var table = document.createElement("table");
+
+					 // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+					 var tr = table.insertRow(-1);                   // TABLE ROW.
+
+					 for (var i = 0; i < col.length; i++) {
+							 var th = document.createElement("th");      // TABLE HEADER.
+							 th.innerHTML = col[i];
+							 tr.appendChild(th);
+					 }
+
+					 // ADD JSON DATA TO THE TABLE AS ROWS.
+					 for (var i = 0; i < arrItems.length; i++) {
+
+							 tr = table.insertRow(-1);
+
+							 for (var j = 0; j < col.length; j++) {
+									 var tabCell = tr.insertCell(-1);
+									 tabCell.innerHTML = arrItems[i][col[j]];
+							 }
+					 }
+
+					 // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+					 var divContainer = document.getElementById("showData");
+					 divContainer.innerHTML = "";
+					 divContainer.appendChild(table);
+			 });
+	 };
+*/
--- app/js/leafletembed.js
+++ app/js/leafletembed.js
@@ -1,51 +1,73 @@
 var map = L.map('map');
 map.setView([51.2, 7], 9);
+/*addPreview(); */
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; OpenStreetMap contributors'
-}).addTo(map);
+var whiteAndBlack =
+        L.tileLayer('//{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            subdomains: 'abcd',
+            maxZoom: 20,
+            minZoom: 0,
+            label: 'White and Black'  // optional label used for tooltip
+        }),
+    osm =
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            label: 'Street Map',
+            maxZoom: 18,
+            attribution: 'Map data &copy; OpenStreetMap contributors'
+        }).addTo(map);
+
+var lyr = L.tileLayer('./{z}/{x}/{y}.png', {tms: true, opacity: 0.7, attribution: ""});
+
+var baseMaps = {
+    'Street Map': osm,
+    'White and Black': whiteAndBlack
+};
+
+var overlaymaps = {
+    "Layer": lyr
+}
+
+L.control.layers(baseMaps, overlaymaps).addTo(map);
 
 var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 // FeatureGroup is to store editable layers
 var drawnItems = new L.FeatureGroup();
-console.log(drawnItems)
 map.addLayer(drawnItems);
+
 var drawControl = new L.Control.Draw({
-  draw: {
-    polyline: false,
-    polygon: false,
-    marker: false,
-    circle: false,
-    circlemarker: false,
-  },
-  edit: {
-    featureGroup: drawnItems
-  }
+    draw: {
+        polyline: false,
+        polygon: false,
+        marker: false,
+        circle: false,
+        circlemarker: false,
+    },
+    edit: {
+        featureGroup: drawnItems
+    }
 });
 
 //add event handlers for drawing on Map and save coordinates into an array 
 
-var recCoord
+var recCoord;
 map.on(L.Draw.Event.CREATED, function (e) {
 
-  drawnItems.clearLayers();
+    drawnItems.clearLayers();
 
-   var type = e.layerType;
-   var layer = e.layer
-   console.log(layer);
+    var type = e.layerType;
+    var layer = e.layer
+    console.log(layer);
 
-   drawnItems.addLayer(layer);
+    drawnItems.addLayer(layer);
     if (type === 'rectangle') {
-    recCoord = layer._latlngs[0];
+        recCoord = layer._latlngs[0];
 
-   }
+    }
     console.log(recCoord)
 
 });
-
-
 
 
 map.addControl(drawControl);
@@ -54,13 +76,13 @@ var toolbar = L.Toolbar();
 toolbar.addToolbar(map);
 
 var modifiedDraw = L.drawLocal.extend({
-  draw: {
-    toolbar: {
-      buttons: {
-          rectangle: 'Draw a recangle on the map'
+    draw: {
+        toolbar: {
+            buttons: {
+                rectangle: 'Draw a recangle on the map'
+            }
         }
-      }
     }
-  });
+});
 
-  map.addControl(drawControl);
+map.addControl(drawControl);
